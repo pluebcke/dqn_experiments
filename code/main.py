249270dfa35379372dc004d1d14bfd7c37a8549f
Peter Lübcke <42319502+pluebcke@users.noreply.github.com>
2020-05-12 20:35:40 +0200

    main file for the DistributionalDQN agent

--- code/main.py
+++ code/main.py
@@ -6,7 +6,7 @@ from bsuite.baselines.utils import pool
 from agent import Agent
 import torch
 
-save_path = "./runs/run028/"  # Path were the results are saved.
+save_path = "./runs/run032/"  # Path were the results are saved.
 
 
 def run(bsuite_id: str) -> str:
@@ -28,14 +28,14 @@ def run(bsuite_id: str) -> str:
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     # Settings for the neural network
-    qnet_settings = {"layers_sizes": [50], "batch_size": 64, "noisy_nets": True}
+    qnet_settings = {'layers_sizes': [50], 'batch_size': 64, 'noisy_nets': True, 'distributional': True, 'vmin': 0,
+                     'vmax': 1000, 'number_atoms': 51}
 
     # Settings for the specific agent
-    settings = {"batch_size": qnet_settings["batch_size"], "epsilon_start": 0.0, "epsilon_decay": 0.00,
-                "epsilon_min": 0.00, "gamma": 0.99, "buffer_size": 2 ** 16, "lr": 1e-3, "qnet_settings": qnet_settings,
-                "start_optimization": 64, "update_qnet_every": 2, "update_target_every": 50,
-                "ddqn": True, "n_steps": 4, "duelling_dqn": True, "prioritized_buffer": True, "alpha": 0.6,
-                "beta0": 0.4, "beta_increment": 1e-6}
+    settings = {'batch_size': qnet_settings["batch_size"], 'epsilon_start': 0.0, 'epsilon_decay': 0.00,
+                'epsilon_min': 0.00, 'gamma': 0.99, 'buffer_size': 2 ** 16, 'lr': 1e-3, 'qnet_settings': qnet_settings,
+                'start_optimization': 64, 'update_qnet_every': 2, 'update_target_every': 50, 'ddqn': True, 'n_steps': 4,
+                'duelling_dqn': True, 'prioritized_buffer': True, 'alpha': 0.6, 'beta0': 0.4, 'beta_increment': 1e-6}
 
     agent = Agent(action_spec=env.action_spec(),
                   observation_spec=env.observation_spec(),
@@ -50,5 +50,10 @@ def run(bsuite_id: str) -> str:
         verbose=False)
     return bsuite_id
 
-bsuite_sweep = getattr(sweep, 'SWEEP')
+
+bsuite_sweep = getattr(sweep, 'CARTPOLE_SCALE')
 pool.map_mpi(run, bsuite_sweep, 6)
+
+
+#bsuite_sweep = getattr(sweep, 'SWEEP')
+#pool.map_mpi(run, bsuite_sweep, 6)
